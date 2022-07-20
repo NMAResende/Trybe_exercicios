dd8390e3cfc89620905c58452a2b433afd722ad4
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-20 19:34:15 -0300

    Criando o programa maior de 3

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maiorde3.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 50;
+const c = 4;
+
+if (a > b && a > c){
+    console.log(a);
+}
+else if (b > a && b > c)
+{
+    console.log(b);
+
+}else if (c > a && c > b){
+    console.log(c);
+}
