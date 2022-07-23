d52e5709c305f72f1515ac5b6d3b0cbc30cd6d02
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-22 21:23:23 -0300

    Criando a pirâmide de asteriscos

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exerciciobonus3.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exerciciobonus3.js
@@ -1,9 +1,9 @@
 let n = 5;
 let aux = '';
-let posicao = n-1;
+let posicao = n;
 
-for (let i = 0; i < n; i += 1){
-    for (let j = 0; j < n; j += 1){
+for (let i = 1; i <= n; i += 1){
+    for (let j = 1; j <= n; j += 1){
         if (j < posicao){
             aux += ' ';
         }
