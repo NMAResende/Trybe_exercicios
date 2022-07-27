7f49ea427d94ce2d9a5ff246e94519b67c19b961
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-27 19:46:47 -0300

    Praticando querySelector e getElement...

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,17 @@
+const adm = document.getElementById('header-container')
+adm.style.backgroundColor='rgb(0, 176, 105)';
+adm.style.color='white';
+adm.style.textAlign="center";
+
+const corPurple = document.getElementsByClassName('emergency-tasks')[0]
+corPurple.style.backgroundColor='orange';
+
+
+const corBlack = document.getElementsByClassName('no-emergency-tasks')[0]
+corBlack.style.backgroundColor='yellow';
+corBlack.style.color='white';
+
+const footer = document.getElementById("footer-container")
+footer.style.backgroundColor="rgb(0, 53, 51)";
+footer.style.color="white";
+footer.style.textAlign='center';
