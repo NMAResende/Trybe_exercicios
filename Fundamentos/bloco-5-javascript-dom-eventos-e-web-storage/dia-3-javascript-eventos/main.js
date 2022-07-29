d936b18854bc6ab2339c17234aa86ae49cbb86ef
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-29 16:23:57 -0300

    Criando função para add classe

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -9,13 +9,28 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
-firstLi.style.marginTop ='22px'
+firstLi.style.marginTop ='22px';
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function addClass(event){
+    const tech = document.getElementsByClassName('tech')[0];
+    tech.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value ='';
+}
+firstLi.addEventListener('click', addClass);
+secondLi.addEventListener('click', addClass);
+thirdLi.addEventListener('click', addClass);
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('input', function(event){
+    const textBox = document.getElementsByClassName('tech')[0];
+    textBox.innerText =event.target.value
+});
+console.log(input)
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
