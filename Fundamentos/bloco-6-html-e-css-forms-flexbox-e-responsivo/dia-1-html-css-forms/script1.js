0c246960c59b2cadb700d42e04a5ba2573b94ab1
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-04 20:03:43 -0300

    Realizando o exercício proposto

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script1.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script1.js
@@ -1,10 +1,8 @@
-
-
 function getButton(event){
     event.preventDefault();
     }
-    window.onload = function (){
-        const botaoEnviar = document.querySelector("#botao-enviar");
+    window.onload = function () {
+        const botaoEnviar = document.getElementById('botao-enviar');
         botaoEnviar.addEventListener('click', getButton);
     }
     
