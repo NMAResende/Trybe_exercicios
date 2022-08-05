498ca7ae261cfa0a6dc40c9d98e5e92c8c5282e6
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-05 20:19:23 -0300

    Criando autorização de fotos

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script1.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script1.js
@@ -5,21 +5,32 @@ function getButton(event){
         const botaoEnviar = document.getElementById('botao-enviar');
         botaoEnviar.addEventListener('click', getButton);
     }
-    
+
 function botaoApagar(){
     const apagar = document.getElementById('botao-apagar');
 
     apagar.addEventListener('click', botaoApagar);
 }
-
 function clearButton(){
     const botaoLimpar = document.querySelectorAll("input");
-    
     for(let i = 0; i < botaoLimpar.length; i += 0){
         const analisarInput = botaoLimpar[i];
         if(analisarInput.value = ''){
             analisarInput.checked = false;
         };
-
     }
 }
+// Bônus
+function authorization(){
+    const autImg = document.getElementById('divulgacaoFoto');
+    
+    autImg.change(function(event) {
+        let autImg = event.target;
+        if (autImg.checked) {
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
+})
+}
+autImg.addEventListener('click', authorization);
