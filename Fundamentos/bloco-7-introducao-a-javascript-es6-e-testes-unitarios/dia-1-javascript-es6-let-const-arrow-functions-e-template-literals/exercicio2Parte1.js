a641515c04bb5954ae6b10897e3fb49e847f0657
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-12 19:36:36 -0300

    Realizando o exercicio22

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio2Parte1.js
@@ -0,0 +1,7 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = oddsAndEvens.sort((a, b) => a - b);
+
+
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); 
