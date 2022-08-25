98061b5d308c85d3aeeda2ed7a83047e60a5522e
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-25 12:56:18 -0300

    Realizando os exercícios 5 e 6

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -0,0 +1,20 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, curr) =>{
+    const currLetraA = curr.split('');
+    const letraA = currLetraA.map((letra) => {
+      if(letra === 'A' || letra === 'a'){
+        acc++;
+    }
+    })
+    return acc
+  },0)
+  
+  
+}
+console.log(containsA());
